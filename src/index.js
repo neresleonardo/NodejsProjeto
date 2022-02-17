9d74d2c4032042968312f14e694ef4087bf0e352
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-16 23:30:06 -0300

    Creating account deposit

--- src/index.js
+++ src/index.js
@@ -8,16 +8,26 @@ const app = express();
 
 
 app.use(express.json());
-/**
- * cpf - string 
- * name - string
- * id - uuid
- * statement []
- * 
- */
+
 
 const customers = [];
 
+//Middleware
+function verifyExistsAccountCPF(req,res,next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+                    //Procurando se existe um cpf estritamente parecido
+
+                    
+    if(!customer) {
+        return res.status(400).json({error: "Customer not found"})
+    }
+
+    req.customer = customer;
+
+    return next();
+    }
 
 // Creating an account and validating the cpf
 app.post("/account", (req, res) => {
@@ -42,17 +52,27 @@ app.post("/account", (req, res) => {
 })
 
 // Looking for the customer's bank statement
-app.get("/statement/:cpf", (req,res) => {
-    const { cpf } = req.params;
+app.get("/statement/", verifyExistsAccountCPF, (req,res) => {
 
-    const customer = customers.find((customer) => customer.cpf === cpf);
-                    //Procurando se existe um cpf estritamente parecido
+    const { customer } = req;
+    return res.json(customer.statement);
+})
 
-    if(!customer) {
-        return res.status(400).json({error: "Customer not found"})
+app.post("/deposit", verifyExistsAccountCPF, (req,res) => {
+    const { description, amount } = req.body; 
+
+    const { customer } = req;
+
+    const statementOperations = {
+        description,
+        amount,
+        createdAt: new Date(),
+        type: "credit"
     }
 
-    return res.json(customer.statement);
+    customer.statement.push(statementOperations)
+
+    return res.status(201).send()
 })
 
 app.listen(3333, () => {
