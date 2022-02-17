7661c1bf156e28f74f579df51aa1e06f15e07ea3
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-16 21:31:49 -0300

    Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-const express = require("express")
+const { response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid"); // v4 números rendon
+
+
 
 const app = express();
 
+app.use(express.json());
+/**
+ * cpf - string 
+ * name - string
+ * id - uuid
+ * statement []
+ * 
+ */
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return res.status(201).send();  // 201 status de banco criado
+})
 
 app.listen(3333, () => {
    console.log( "Correndo em 3333");
